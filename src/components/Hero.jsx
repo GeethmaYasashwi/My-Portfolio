d311de23bd6f23d59a914d15896aae24117d18ce
GeethmaYasashwi <geethmayasaswi@gmail.com>
2026-07-11 15:14:21 +0530

    bug fix: cv upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ export default function Hero({ onNavLinkClick }) {
                         View Projects
                     </a>
                     <a
-                        href="public/assests/Geethma_Yasashwi-CV.pdf"
+                        href="public/assests/GeethmaYasashwi-CV.pdf"
                         className="cta-button secondary"
                         download
                     >
