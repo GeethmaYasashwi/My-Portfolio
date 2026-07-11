0adfa5a0e5e592440fbe6fc9100b73e30c14b63c
GeethmaYasashwi <geethmayasaswi@gmail.com>
2026-07-11 15:49:02 +0530

    bugs fix: update file path of cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ export default function Hero({ onNavLinkClick }) {
                         View Projects
                     </a>
                 <a
-                href="/assets/GeethmaYasashwi-CV.pdf"
+                href="/assests/GeethmaYasashwi-CV.pdf"
                 className="cta-button secondary"
                 download="GeethmaYasashwi-CV.pdf"
                 >
